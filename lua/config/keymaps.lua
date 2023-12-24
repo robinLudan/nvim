d519db0d9692cbe385e34e767f9b78f9c19a6c92
Robin Ludan <robinludan@gmx.com>
2023-12-24 21:04:10 +0200

    custom keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,27 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- general keymaps
+keymap.set("i", "jk", "<ESC>")
+
+-- select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+-- new tab
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+
+keymap.set("n", "<leader>nh", ":nohl<CR>", opts)
+
+keymap.set("n", "x", '"_x')
+
+keymap.set("n", "<leader>+", "<C-a>")
+keymap.set("n", "<leader>-", "<C-x>")
+
+-- split windows
+keymap.set("n", "<leader>sv", "<C-w>v", opts)
+keymap.set("n", "<leader>sh", "<C-w>s", opts)
+keymap.set("n", "<leader>se", "<C-w>=", opts)
+keymap.set("n", "<leader>sx", ":close<CR>", opts)
