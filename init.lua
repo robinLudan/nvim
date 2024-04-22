1197342733f4f71d1b18cdf89d6f02c15482bda3
Robin <robinludan@gmx.com>
2024-04-22 08:01:29 +0300

    diagnostics configuration

--- init.lua
+++ init.lua
@@ -523,6 +523,20 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          -- Hide diagnostics virtual text (show it with <space>d, including it's source)
+          vim.diagnostic.config {
+            virtual_text = false,
+            float = {
+              source = true,
+            },
+          }
+
+          -- Diagnostic symbols
+          vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+          vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
+          vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+          vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
