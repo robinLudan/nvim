5371c7d7be9bca59cb955cd1396d786dbc5acd21
Robin Ludan <robinludan@gmx.com>
2024-08-21 21:02:25 +0300

    macros shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -39,3 +39,7 @@ map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- playback macros
+map('n', 'Q', '@qj', opts)
+map('x', 'Q', ':norm @q<CR>', opts)
