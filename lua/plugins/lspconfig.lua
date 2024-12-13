a0349b85afe00a57aa3ce981570c209e6f503498
Robin Ludan <robinludan@gmx.com>
2024-12-13 18:30:52 +0300

    add ts lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -209,7 +209,7 @@ return { -- LSP Configuration & Plugins
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`tsserver`) will work just fine
-      -- ts_ls = {},
+      ts_ls = {},
       --
 
       lua_ls = {
